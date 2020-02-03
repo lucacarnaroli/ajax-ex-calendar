81d4fa5df2efecdca5186c81a7402454255d73bc
luca <luca.carnaroli98@gmail.com>
2020-02-03 17:52:54 +0100

    stampo i giorni

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+  for (var i = 1; i <= 31; i++) {
+   $('.days').append(' '+ i + ' ');
+  }
+
+
   $.ajax(
     {
     'url': "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
     'method': "GET",
-    'success': function (data, name) {
-    // $("#risultati").html(data);
-    console.log(data, name);
+    'data': {
+      year: 2018,
+      month : 0,
     },
-    'error': function (richiesta, stato, errori) { alert("E' avvenuto un errore. " + errore);
+    'success': function(date,stato) {
+      console.log(date.response);
+    },
+    'error': function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
       }
     }
   );
-  
+
 });
