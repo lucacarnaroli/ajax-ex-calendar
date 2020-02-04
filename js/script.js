2acc79026fdf790cd133e2c3d53ff66085b3823b
luca <luca.carnaroli98@gmail.com>
2020-02-04 20:46:33 +0100

    errore nel next month

--- js/script.js
+++ js/script.js
@@ -1,41 +1,56 @@
 $(document).ready(function() {
-  var mese = 1;
-  var url = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month='+ (mese-1);
+  var meseIniziale = 0;
+  var anno = 2018;
+  var mese = moment({
+    year: anno,
+    month: meseIniziale
+  });
+
   calcMesi(mese);
+  giorniFestivi(mese);
 
-  $.ajax(
-    {
-    'url': url ,
-    'method': "GET",
-    'success': function(data) {
-      for (var i = 0; i < data.response.length; i++) {
-        $('li').each(function() {
-          if (data.response[i].date == $(this).attr('data')) {
-            $(this).addClass('red');
-          }
-        });
-      };
-    },
-    'error': function (richiesta, stato, errori) {
-      alert("E' avvenuto un errore. " + errore);
-    }
+  function giorniFestivi(mese) {
+    $.ajax(
+      {
+      'url': 'https://flynn.boolean.careers/exercises/api/holidays',
+      'method': "GET",
+      'data': {
+        year : mese.year(),
+        month : mese.month()
+      },
+      'success': function(data) {
+        console.log(data);
+        for (var i = 0; i < data.response.length; i++) {
+          $('li').each(function() {
+            if (data.response[i].date == $(this).attr('data-complete-date')) {
+              $(this).addClass('red');
+              $(this).children('.festivita').append(data.response[i].name);
+            }
+          });
+        };
+      },
+      'error': function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+    });
   }
-  );
-
-
 });
 
 // funzioni
 function calcMesi(mese) {
-  var daysMonth = moment('2018-' + mese).daysInMonth();
+  $('.mese').text(mese.format('MMMM-YYYY'));
+  // var thisMonth = $('.mese').attr('data-this-month', mese.format('YYYY-MMM'));
+  $('.mese').attr('data-this-month',mese.format('YYYY-MM'));
+  $('.days').html('');
+  var daysMonth = mese.daysInMonth();
   console.log(daysMonth);
-  for (var i = 0; i < daysMonth; i++) {
+  for (var i = 1; i <= daysMonth; i++) {
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
     var context = {
-      data : i+1,
-      mese : moment().month(mese -1).format('MMM'),
-      attr : '2018-' + addZero(mese) + '-' + addZero(i+1)
+      'data' : addZero(i),
+      'mese' : mese.format('MMMM'),
+      'complete-date' : '2018-' + mese.format('MM') + '-' + addZero(i)
     };
     var html = template(context);
     $('.days').append(html);
@@ -49,3 +64,21 @@ function addZero(num) {
     return num
   }
 }
+
+$('.next').click(function() {
+  var thisMonth = $('.mese').attr('data-this-month');
+  var date = moment(thisMonth).add(1, 'months');
+
+  calcMesi(date);
+  giorniFestivi(date);
+
+});
+
+$('.prev').click(function() {
+  var thisMonth = $('.mese').attr('data-this-month');
+  var date = moment(thisMonth).subtract(1, 'months');
+
+  calcMesi(date);
+  giorniFestivi(date);
+
+});
